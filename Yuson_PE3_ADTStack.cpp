79a876e3a01f04249dc1fd20484393f002941294
ikeman09 <iggygarena@gmail.com>
2021-10-22 04:23:26 +0800

    for passing code

---
+++ Yuson_PE3_ADTStack.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ikegabrielyuson on 10/22/21.
+//
+#include <iostream>
+
+using namespace std;
+
+class Node{
+public:
+    string item[20];
+    Node* next;
+};
+
+int main(){
+
+}
